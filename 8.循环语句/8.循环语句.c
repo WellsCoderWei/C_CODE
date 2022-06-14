cf0fbdfced4845c28e3441024b2c53d2febc6f83
WellsCoderWei <107259525+WellsCoderWei@users.noreply.github.com>
2022-06-14 09:55:31 +0800

    Add files via upload

---
+++ 8.循环语句/8.循环语句.c
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	int line = 1; //计数
+//	while (line <= 3000) {
+//		printf("写代码:%d\n", line);
+//		line++;
+//	}
+//	if (line > 3000) {
+//		printf("好offer");
+//	}
+//	return 0;
+//}
+
+int main()
+{
+	printf("加入比特\n");
+	int line = 0;
+	while (line <= 2000) {
+		line++;
+		printf("我要继续努力敲代码:%d\n", line);
+	}
+	if (line > 2000) {
+		printf("迎娶白富美\n");
+	}
+	return 0;
+}
