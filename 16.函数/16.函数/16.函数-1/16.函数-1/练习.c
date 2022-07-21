9792662c4f9b0da191b44aa3e9a96a5a0d2699ab
WellsCoderWei <107259525+WellsCoderWei@users.noreply.github.com>
2022-07-21 15:00:11 +0800

    Add files via upload

---
+++ 16.函数/16.函数/16.函数-1/16.函数-1/练习.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//写一个函数交换两个整型变量的值
+//void swap(int* px, int* py)
+//{
+//	int z = 0;
+//	z = *px;
+//	*px = *py;
+//	*py = z;
+//}
+//int main()
+//{
+//	int num1 = 10;
+//	int num2 = 20;
+//	printf("交换前：num1=%d num2=%d\n", num1, num2);
+//	swap(&num1, &num2);
+//	printf("交换后：num1=%d num2=%d\n", num1, num2);
+//	return 0;
+//}
